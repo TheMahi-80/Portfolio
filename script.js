b9e9f15422f5ed05ecaab576a3c5a2da76c230ed
TheMahi-80 <montasin8110@gmail.com>
2026-03-06 21:01:29 +0600

    first portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const section = document.querySelector(link.getAttribute("href"));
+
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
